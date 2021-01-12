c61eb00b768b18138705f19907e48d6d295f5fc0
Anton Zhulanov <zhulanoff2007@gmail.com>
2021-01-13 00:20:24 +0300

    add str tests

---
+++ tests/test_str.py
@@ -0,0 +1,32 @@
+import pytest
+
+
+@pytest.fixture()
+def empty_str():
+    return str()
+
+
+@pytest.fixture()
+def not_empty_str():
+    return "string"
+
+
+def test_empty_str_len(empty_str):
+    assert len(empty_str) == 0
+
+
+def test_not_empty_str_len(not_empty_str):
+    assert len(not_empty_str) == 6
+
+
+def test_starts_with_letter(not_empty_str):
+    assert not_empty_str.startswith("str")
+    assert not not_empty_str.startswith("qwe")
+
+
+def test_upper(not_empty_str):
+    assert not_empty_str.upper() == "STRING"
+
+
+def test_capitalize(not_empty_str):
+    assert not_empty_str.capitalize() == "String"
