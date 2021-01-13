1c27e740ba68f886886472011f3b98bbcffc5ce1
Anton Zhulanov <zhulanoff2007@gmail.com>
2021-01-13 11:31:00 +0300

    add test letter in string

--- tests/test_str.py
+++ tests/test_str.py
@@ -15,13 +15,18 @@ def test_empty_str_len(empty_str):
     assert len(empty_str) == 0
 
 
-def test_not_empty_str_len(not_empty_str):
+def test_not_empty_str_more_than_zero(not_empty_str):
     assert len(not_empty_str) == 6
 
 
-def test_starts_with_letter(not_empty_str):
+def test_starts_with_symbols(not_empty_str):
     assert not_empty_str.startswith("str")
     assert not not_empty_str.startswith("qwe")
+
+
+def test_letter_in_string(not_empty_str):
+    assert "s" in not_empty_str
+    assert "q" not in not_empty_str
 
 
 def test_upper(not_empty_str):
