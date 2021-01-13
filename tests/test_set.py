2ae7e82c872e91090141e63625c9e6ebc4eb9666
Anton Zhulanov <zhulanoff2007@gmail.com>
2021-01-13 12:11:32 +0300

    add param test remove of set

--- tests/test_set.py
+++ tests/test_set.py
@@ -29,7 +29,7 @@ def test_unique_items(not_empty_set):
     new_not_empty_set = not_empty_set.copy()
     item_set = list(not_empty_set)[-1]
     new_not_empty_set.add(item_set)
-    new_not_empty_set == not_empty_set
+    assert new_not_empty_set == not_empty_set
 
 
 def test_pop_item(not_empty_set):
@@ -40,10 +40,14 @@ def test_pop_item(not_empty_set):
     assert pop_set_item not in not_empty_set
 
 
-def test_remove(not_empty_set):
-    set_item = list(not_empty_set)[0]
-    not_empty_set.remove(set_item)
-    assert set_item not in not_empty_set
+@pytest.mark.parametrize(
+    "new_not_empty_set, item",
+    [({1, 2, 3}, 1), ({"1", "2", "3"}, "2"), ({1, "2", 3}, 3)]
+)
+def test_remove(new_not_empty_set, item):
+    assert item in new_not_empty_set
+    new_not_empty_set.remove(item)
+    assert item not in new_not_empty_set
 
 
 def test_clear(not_empty_set):
